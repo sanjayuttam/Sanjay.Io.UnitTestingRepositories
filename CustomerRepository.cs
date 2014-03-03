fb7fde573a85406c986e82946ad21fe61ef07f71
sanjayuttam <sanjay.uttam@gmail.com>
2014-03-03 17:12:47 -0500

    Initial add

---
+++ CustomerRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.DataAnnotations;
+using ServiceStack.OrmLite;
+
+namespace Sanjay.Io.UnitTestingRepositories
+{
+    public class CustomerRepository
+    {
+        private readonly IDbConnectionFactory _dbFactory = null;
+        private const string GetCustomerQuery = "SELECT Id, FirstName, LastName FROM Customer WHERE Id = {0}";
+
+        public CustomerRepository(IDbConnectionFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+
+        public Customer Get(int id)
+        {
+            using (IDbConnection db = _dbFactory.OpenDbConnection())
+            {
+                return db.Select<Customer>(GetCustomerQuery, id).SingleOrDefault();
+            }
+        }
+    }
+
+    public class Customer
+    {
+        [Index]
+        [AutoIncrement]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ Tests/Sanjay.Io.UnitTestingRepositories.Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Funq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceStack.OrmLite;
+
+namespace Sanjay.Io.UnitTestingRepositories.Tests
+{
+    [TestClass]
+    public class CustomerRepositoryTests
+    {
+        public Container Container { get; set; }
+        public IDbConnectionFactory DbFactory { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            Container = new Container();
+            Container.Register<IDbConnectionFactory>(
+                new OrmLiteConnectionFactory(":memory:", false, SqliteDialect.Provider));
+            DbFactory = Container.Resolve<IDbConnectionFactory>();
+
+            using (var db = DbFactory.Open())
+            {
+                db.CreateTable<Customer>(overwrite: true);
+                db.Insert(new Customer
+                {
+                    FirstName = "Sanjay",
+                    LastName = "Uttam"
+                });
+            }
+        }
+
+        [TestMethod]
+        public void Test_Get_Customer_By_Id()
+        {
+            var result = new CustomerRepository(DbFactory).Get(1);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
